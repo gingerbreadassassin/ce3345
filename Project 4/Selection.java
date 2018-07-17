01b50b3b5437382c410a9de40796c11fa90f2346
gingerbreadassassin <connordvn@gmail.com>
2018-07-16 19:20:18 -0500

    Project 4 done

--- Project 4/Selection.java
+++ Project 4/Selection.java
@@ -1,2 +1,78 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Selection {
+
+    private Integer[] list = {-4598,-5679,-3231,-9457,-2822,-3457,-6371,-8743};
+    private BinaryHeap<Integer> heap = new BinaryHeap<>(list);
+
+    public Selection(int n) {
+        printList();
+        findIt(n);
+    }
+
+    public Selection(int n, int m){
+        list = new Integer[m];
+        for(int i = 0; i< m; i++){
+            list[i] = -i;
+        }
+        Collections.shuffle(Arrays.asList(list));
+        heap = new BinaryHeap<>(list);
+        printList();
+        findIt(n);
+    }
+
+    private void printList(){
+        System.out.println("Printing the list: ");
+        for(int e : list) {
+            System.out.println(-e);
+        }
+        System.out.println();
+    }
+
+    private void findIt(int n) {
+        int i = 0;
+        int l = n;
+        while(n-- > 0){
+            i = heap.deleteMin();
+        }
+        System.out.println(String.format("The %d largest element is: %d", l, -i));
+    }
+
+    public static void main (String[] args) {
+        if(args.length == 0) {
+            System.out.println("Usage: java Selection [n] [m]");
+            System.out.println("This program selects the nth largest number from a list.");
+            System.out.println("A default list is used if only \"n\" is specified.");
+            System.out.println("Otherwise, you may specify a list of size 1-9999 for \"m\"");
+            System.out.println("which will randomly generate a list of that length.");
+        } else {
+            if(args.length == 1) {
+                try {
+                    int n = Integer.parseInt(args[0]);
+                    if(n >= 1 && n <= 8){
+                        Selection s = new Selection(n);
+                    } else {
+                        System.out.println("Please choose n between 1 and 8 for default run!");
+                        return;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("You didn't enter a number I could understand.");
+                }
+            } else if(args.length == 2) {
+                try {
+                    int n = Integer.parseInt(args[0]);
+                    int m = Integer.parseInt(args[1]);
+                    if((n >= 1 && n <= m) && (m <= 9999)){
+                        Selection s = new Selection(n, m);
+                    } else {
+                        System.out.println("Please choose n between 1 and m, and m between n and 9999 for random run!");
+                        return;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("You didn't enter a number I could understand.");
+                }
+            }
+        }
+    }
 }
